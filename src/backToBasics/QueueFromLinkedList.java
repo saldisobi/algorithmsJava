374d0f1ffd3d44fc753a212370a05e1de5c936fe
sourabh saldi <saldisaurabh99@gmail.com>
2020-03-28 11:55:52 +0530

    queue

---
+++ src/backToBasics/QueueFromLinkedList.java
@@ -0,0 +1,87 @@
+package backToBasics;
+
+public class QueueFromLinkedList {
+
+    Node head;
+
+    Node tail;
+
+    class Node {
+        int data;
+        Node previous;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+
+        Node(int data, Node prev) {
+            this.data = data;
+            this.previous = prev;
+        }
+    }
+
+
+    void push(int data) {
+
+        if (head == null) {
+            Node node = new Node(data);
+            head = node;
+            tail = node;
+        } else {
+            Node node = new Node(data, head);
+            head.next = node;
+            head = node;
+        }
+    }
+
+    int peek() {
+
+        System.out.println(tail.data + "");
+        return tail.data;
+    }
+
+    void pop() {
+        tail = tail.next;
+    }
+
+    public static void main(String args[]) {
+        QueueFromLinkedList stackFromLinkedList = new QueueFromLinkedList();
+        stackFromLinkedList.push(1);
+        stackFromLinkedList.push(12);
+        stackFromLinkedList.push(31);
+        stackFromLinkedList.push(12);
+        stackFromLinkedList.push(13);
+
+        stackFromLinkedList.peek();
+
+        stackFromLinkedList.pop();
+
+        stackFromLinkedList.peek();
+
+        stackFromLinkedList.push(222);
+
+        stackFromLinkedList.peek();
+
+        stackFromLinkedList.pop();
+
+        stackFromLinkedList.pop();
+
+        stackFromLinkedList.peek();
+
+        stackFromLinkedList.pop();
+
+        stackFromLinkedList.peek();
+
+        stackFromLinkedList.pop();
+
+        stackFromLinkedList.peek();
+
+        stackFromLinkedList.pop();
+
+        stackFromLinkedList.peek();
+
+
+
+    }
+}
---
+++ src/backToBasics/StackFromLinkedList.java
@@ -0,0 +1,63 @@
+package backToBasics;
+
+public class StackFromLinkedList {
+
+    Node top;
+
+    class Node {
+        int data;
+        Node previous;
+
+        Node(int data) {
+            this.data = data;
+        }
+
+        Node(int data, Node prev) {
+            this.data = data;
+            this.previous = prev;
+        }
+    }
+
+
+    void push(int data) {
+
+        if (top == null) {
+            Node node = new Node(data);
+            top = node;
+        } else {
+            Node node = new Node(data, top);
+            top = node;
+        }
+    }
+
+    int peek() {
+
+        System.out.println(top.data + "");
+        return top.data;
+    }
+
+    void pop() {
+        top = top.previous;
+    }
+
+    public static void main(String args[]) {
+        StackFromLinkedList stackFromLinkedList = new StackFromLinkedList();
+        stackFromLinkedList.push(1);
+        stackFromLinkedList.push(12);
+        stackFromLinkedList.push(31);
+        stackFromLinkedList.push(12);
+        stackFromLinkedList.push(13);
+
+        stackFromLinkedList.peek();
+
+        stackFromLinkedList.pop();
+
+        stackFromLinkedList.peek();
+
+        stackFromLinkedList.push(222);
+
+        stackFromLinkedList.peek();
+
+
+    }
+}
