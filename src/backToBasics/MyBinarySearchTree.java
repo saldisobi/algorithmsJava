a4fd4a0660882b11773bac8d6917fae4f4ee3590
sourabh saldi <saldisaurabh99@gmail.com>
2020-04-04 11:40:44 +0530

    binary search tree

---
+++ src/backToBasics/MyBinarySearchTree.java
@@ -0,0 +1,113 @@
+package backToBasics;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyBinarySearchTree {
+
+    Node rootNode;
+
+    class Node {
+        int data;
+        Node leftChild;
+        Node rightChild;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public void insert(int data) {
+        System.out.println("command to print " + data);
+        Node node = new Node(data);
+        Node current = rootNode;
+        if (rootNode == null) {
+            System.out.println("root node ws null now root ode has data = " + data);
+            rootNode = node;
+            return;
+        }
+        while (current != null) {
+            if (data < current.data) {
+                System.out.println("data " + data + " < " + current.data);
+                if (current.leftChild == null) {
+                    System.out.println("left child was null insertion done");
+                    current.leftChild = node;
+                    return;
+                } else {
+                    System.out.println("left child was not null current is now "+current.leftChild.data);
+                    current = current.leftChild;
+                }
+            } else if (data > current.data) {
+                System.out.println("data " + data + " > " + current.data);
+                if (current.rightChild == null) {
+                    System.out.println("right child was null insertion done");
+
+                    current.rightChild = node;
+                    return;
+                } else {
+                //    System.out.println("right child was not null current is now "+current.leftChild.data);
+                    current = current.rightChild;
+                }
+            } else {
+                throw new RuntimeException("diuplicated not allowed");
+            }
+        }
+    }
+
+    public void levelOrderIterative() {
+
+        Queue<MyBinarySearchTree.Node> myQueue = new LinkedList();
+
+
+        myQueue.add(rootNode);
+        myQueue.add(null);
+
+        int nullCounter = 0;
+        while (!myQueue.isEmpty()) {
+            if (myQueue.peek() != null) {
+                nullCounter = 0;
+                MyBinarySearchTree.Node node = myQueue.peek();
+                myQueue.remove();
+                System.out.print(node.data);
+
+                if (node.leftChild != null) {
+                    myQueue.add(node.leftChild);
+                    myQueue.add(node.rightChild);
+                }
+            } else {
+                nullCounter++;
+                if (nullCounter > 1)
+                    return;
+                myQueue.remove();
+                myQueue.add(null);
+                System.out.println();
+            }
+        }
+
+
+    }
+
+    public static void main(String args[]) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.insert(8);
+
+        tree.insert(3);
+
+        tree.insert(10);
+
+
+        tree.insert(1);
+
+        tree.insert(6);
+
+        tree.insert(14);
+
+        tree.insert(4);
+
+        tree.insert(7);
+
+        tree.insert(13);
+
+        tree.levelOrderIterative();
+    }
+}
--- src/backToBasics/MyTree.java
+++ src/backToBasics/MyTree.java
@@ -1,5 +1,9 @@
 package backToBasics;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class MyTree {
 
     Node rootNode;
@@ -34,11 +38,11 @@ public class MyTree {
 
 
         if (root.leftChild != null)
-            inOrder(root.leftChild);
+            preOrder(root.leftChild);
 
 
         if (root.rightChild != null)
-            inOrder(root.rightChild);
+            preOrder(root.rightChild);
 
     }
 
@@ -47,43 +51,193 @@ public class MyTree {
 
 
         if (root.leftChild != null)
-            inOrder(root.leftChild);
+            postOrder(root.leftChild);
 
 
         if (root.rightChild != null)
-            inOrder(root.rightChild);
+            postOrder(root.rightChild);
 
         System.out.println(root.data);
 
     }
 
+
+    public void levelOrderIterative() {
+
+        Queue<Node> myQueue = new LinkedList();
+
+
+        myQueue.add(rootNode);
+        myQueue.add(null);
+
+        int nullCounter = 0;
+        while (!myQueue.isEmpty()) {
+            if (myQueue.peek() != null) {
+                nullCounter = 0;
+                Node node = myQueue.peek();
+                myQueue.remove();
+                System.out.print(node.data);
+
+                if (node.leftChild != null) {
+                    myQueue.add(node.leftChild);
+                    myQueue.add(node.rightChild);
+                }
+            } else {
+                nullCounter++;
+                if (nullCounter > 1)
+                    return;
+                myQueue.remove();
+                myQueue.add(null);
+                System.out.println();
+            }
+        }
+
+
+    }
+
+
+    public void preOrderIterative() {
+        Stack<Node> myStack = new Stack<>();
+        myStack.push(rootNode);
+
+        while ((!myStack.isEmpty())) {
+            Node node = myStack.pop();
+            System.out.println(node.data);
+            if (node.leftChild != null) {
+                myStack.push(node.rightChild);
+                myStack.push(node.leftChild);
+            }
+        }
+    }
+
+    public void inOrderIterative() {
+        Stack<Node> myStack = new Stack<>();
+
+        Node currentNode = rootNode;
+
+
+        while (currentNode != null || (!myStack.isEmpty())) {
+
+
+            while (currentNode != null) {
+
+                myStack.push(currentNode);
+
+
+                currentNode = currentNode.leftChild;
+
+            }
+            currentNode = myStack.pop();
+
+            System.out.println(currentNode.data + "");
+
+            currentNode = currentNode.rightChild;
+
+
+        }
+
+    }
+
+
+    private void postOrderIterative() {
+
+
+        Stack<Node> myStack = new Stack<>();
+
+        Node currentNode = rootNode;
+
+        while (currentNode != null || !myStack.isEmpty()) {
+            while (currentNode != null) {
+                currentNode = currentNode.leftChild;
+            }
+        }
+    }
+
+
+    public void practiceTwoInOrderIterative() {
+
+        Stack<Node> myStack = new Stack<>();
+
+        Node currentNode = rootNode;
+
+        while (currentNode != null || !myStack.isEmpty()) {
+
+            while (currentNode != null) {
+                myStack.push(currentNode);
+                currentNode = currentNode.leftChild;
+            }
+
+            Node node = myStack.pop();
+            System.out.println(node.data);
+
+            currentNode = node.rightChild;
+
+
+        }
+
+    }
+
+
+    public void practiceTwoPreOrderIterative() {
+        Stack<Node> myStack = new Stack<Node>();
+        myStack.add(rootNode);
+
+        while (!myStack.isEmpty()) {
+
+            Node m = myStack.pop();
+            System.out.println(m.data);
+            if (m.leftChild != null) {
+                myStack.add(m.rightChild);
+                myStack.add(m.leftChild);
+            }
+
+
+        }
+    }
+
+
+    public void practiceTwoPostOrder() {
+        Stack<Node> myStack = new Stack<>();
+        Node currentNode = rootNode;
+        Node currentVisitedNode = null;
+        while (currentNode != null || !myStack.isEmpty()) {
+            while (currentNode != null) {
+                myStack.push(currentNode);
+                currentNode = currentNode.leftChild;
+            }
+
+
+            Node peekNode = myStack.peek();
+
+            if (peekNode.rightChild != null && peekNode.rightChild != currentVisitedNode) {
+
+                currentNode = peekNode.rightChild;
+
+            } else {
+                System.out.println(peekNode.data);
+                Node visit = myStack.pop();
+                currentVisitedNode = visit;
+            }
+        }
+    }
+
+
     public static void main(String args[]) {
 
-        MyTree myTree = new MyTree();
+        MyTree tree = new MyTree();
 
-        Node node = new Node(1);
+        tree.rootNode = new Node(1);
+        tree.rootNode.leftChild = new Node(2);
+        tree.rootNode.rightChild = new Node(3);
+        tree.rootNode.leftChild.leftChild = new Node(4);
+        tree.rootNode.leftChild.rightChild = new Node(5);
 
-        Node node1 = new Node(2);
+        tree.rootNode.rightChild.leftChild = new Node(6);
 
-        Node node2 = new Node(3);
-
-
-        myTree.rootNode = node;
-
-        myTree.rootNode.leftChild = node1;
-
-        myTree.rootNode.rightChild = node2;
-
-      /*  myTree.rootNode.leftChild.leftChild = new Node(4);
-
-        myTree.rootNode.leftChild.rightChild = new Node(5);
-
-        myTree.rootNode.rightChild.leftChild = new Node(6);
-
-        myTree.rootNode.rightChild.rightChild = new Node(7);*/
+        tree.rootNode.rightChild.rightChild = new Node(7);
 
 
-        myTree.inOrder(myTree.rootNode);
+      /*  myTree.inOrder(myTree.rootNode);
 
         System.out.println();
 
@@ -99,10 +253,39 @@ public class MyTree {
 
         System.out.println();
 
-        myTree.postOrder(myTree.rootNode);
+        myTree.postOrder(myTree.rootNode);*/
+
+        // myTree.levelOrderIterative();
+
+        //tree.preOrder(tree.rootNode);
+/*
+        tree.inOrder(tree.rootNode);
+        System.out.println("now Iterative");
+
+        tree.inOrderIterative();*/
+
+      /*  tree.preOrder(tree.rootNode);
+
+        System.out.println("now Iterative");
+
+        tree.preOrderIterative();*/
+
+       /* tree.postOrder(tree.rootNode);
+
+        System.out.println("now Iterative");
+
+        tree.postOrderIterative();*/
+
+        tree.postOrder(tree.rootNode);
+
+
+        System.out.println("here begind new");
+        tree.practiceTwoPostOrder();
 
 
     }
+
+
 }
 
 
