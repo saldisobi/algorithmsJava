38fa8308ef068681820c5d4c64578f5165048edd
sourabh saldi <saldisaurabh99@gmail.com>
2020-05-02 17:23:33 +0530

    done dana dan

---
+++ src/google/BinaryTreeRightView.java
@@ -0,0 +1,70 @@
+package google;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTreeRightView {
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public void printBinaryTreeRightView(TreeNode treeNode) {
+        if (treeNode == null) {
+            System.out.print("null node");
+            return;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(treeNode);
+        queue.add(null);
+
+        while (!queue.isEmpty()) {
+            TreeNode treeNode1 = queue.remove();
+            if(treeNode1==null)
+                return;
+           
+            if (treeNode1.left != null)
+                queue.add(treeNode1.left);
+            if (treeNode1.right != null)
+                queue.add(treeNode1.right);
+            if (queue.peek() == null) {
+
+
+
+                System.out.println(treeNode1.val);
+                queue.add(null);
+                queue.remove();
+            }
+        }
+
+    }
+
+
+    public static void main(String args[]) {
+        TreeNode treeNode = new TreeNode(1);
+        treeNode.left = new TreeNode(2);
+        treeNode.right = new TreeNode(3);
+        treeNode.left.right = new TreeNode(5);
+        treeNode.right.right = new TreeNode(4);
+
+        new BinaryTreeRightView().printBinaryTreeRightView(treeNode);
+    }
+
+}
--- src/google/SubSets.java
+++ src/google/SubSets.java
@@ -1,4 +1,40 @@
 package google;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubSets {
+
+    public List<List<Integer>> subsets(int[] nums) {
+
+        List<List<Integer>> subsets = new ArrayList<>();
+
+        generateAllSubsets(0, nums, new ArrayList<Integer>(), subsets);
+
+        return subsets;
+
+    }
+
+    private void generateAllSubsets(int index, int[] nums, ArrayList<Integer> current, List<List<Integer>> subsets) {
+        subsets.add(new ArrayList<>(current));
+
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]);
+            generateAllSubsets(i + 1, nums, current, subsets);
+            current.remove(current.size() - 1);
+
+        }
+
+    }
+
+    public static void main(String args[]) {
+        List<List<Integer>> result = new SubSets().subsets(new int[]{1, 2, 3});
+        for (int i = 0; i < result.size(); i++) {
+            System.out.println();
+            for (int j = 0; j < result.get(i).size() - 1; j++) {
+
+                System.out.print(result.get(i).get(j));
+            }
+        }
+    }
 }
