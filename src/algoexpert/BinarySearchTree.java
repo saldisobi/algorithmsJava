7481bc0af05c50b43b1821a5f945285eba0a5506
sourabh saldi <saldisaurabh99@gmail.com>
2020-07-12 11:15:30 +0530

    BST delete

--- src/algoexpert/BinarySearchTree.java
+++ src/algoexpert/BinarySearchTree.java
@@ -1,5 +1,10 @@
 package algoexpert;
 
+/**
+ * Delete done in a naive way to just understand functionality can be improved
+ *
+ * won't work to delete root as of now, need to add a small check
+ */
 public class BinarySearchTree {
 
     BinarySearchTreeNode rootNode;
@@ -69,6 +74,71 @@ public class BinarySearchTree {
         return null;
     }
 
+    public boolean delete(int deleteTerm, BinarySearchTreeNode currentNode) {
+        if (currentNode.leftNode != null && currentNode.leftNode.value == deleteTerm) {
+            if (currentNode.leftNode.leftNode == null && currentNode.leftNode.rightNode == null) {
+                //it is a leaf node that has to be deleted
+                currentNode.leftNode = null;
+                return true;
+            } else if (currentNode.leftNode.leftNode == null && currentNode.leftNode.rightNode != null) {
+                // only one right child can be connected directly to parent
+
+                currentNode.leftNode = currentNode.leftNode.rightNode;
+                return true;
+            } else if (currentNode.leftNode.leftNode != null && currentNode.leftNode.rightNode == null) {
+                // only one left child can be connected directly to parent
+                currentNode.leftNode = currentNode.leftNode.leftNode;
+                return true;
+            } else {
+                //we have full tree on the left
+                BinarySearchTreeNode leftMostInRight = findLeftMostInRightSubTree(currentNode.rightNode);
+                currentNode.rightNode.value = leftMostInRight.leftNode.value;
+                leftMostInRight.leftNode = null;
+                return true;
+            }
+
+        } else if (currentNode.rightNode != null && currentNode.rightNode.value == deleteTerm) {
+            if (currentNode.rightNode.leftNode == null && currentNode.rightNode.rightNode == null) {
+                //it is a leaf node that has to be deleted
+                currentNode.rightNode = null;
+                return true;
+            } else if (currentNode.rightNode.leftNode == null && currentNode.rightNode.rightNode != null) {
+                // only one right child can be connected directly to parent
+
+                currentNode.rightNode = currentNode.rightNode.rightNode;
+                return true;
+            } else if (currentNode.rightNode.leftNode != null && currentNode.rightNode.rightNode == null) {
+                // only one left child can be connected directly to parent
+                currentNode.rightNode = currentNode.rightNode.leftNode;
+                return true;
+            } else {
+                //we have full tree on right
+                BinarySearchTreeNode leftMostInRight = findLeftMostInRightSubTree(currentNode.rightNode);
+                currentNode.rightNode.value = leftMostInRight.leftNode.value;
+                leftMostInRight.leftNode = null;
+                return true;
+            }
+        } else {
+
+            if (currentNode.value > deleteTerm) {
+                currentNode = currentNode.rightNode;
+            } else {
+                currentNode = currentNode.leftNode;
+            }
+
+            return delete(deleteTerm, currentNode);
+        }
+
+    }
+
+    private BinarySearchTreeNode findLeftMostInRightSubTree(BinarySearchTreeNode binarySearchTreeNode) {
+        BinarySearchTreeNode bstNode = binarySearchTreeNode;
+        while (bstNode.leftNode != null) {
+            bstNode = bstNode.leftNode;
+        }
+        return bstNode;
+    }
+
 
     public static void main(String args[]) {
         BinarySearchTree bst = new BinarySearchTree();
@@ -78,6 +148,8 @@ public class BinarySearchTree {
         bst.insert(32);
         bst.insert(3);
         bst.insert(2);
+
+        bst.delete(8,bst.rootNode);
 
         BinarySearchTreeNode bstNode = bst.search(12);
     }
